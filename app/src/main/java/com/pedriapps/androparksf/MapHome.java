5a6385e35bb359e54b1a06292010313531a23ec0
Pedro Garate <pedro.garate@yahoo.com>
2015-10-09 03:19:58 -0700

    Update to MapHome.java -Now the map centers on user's location at the start of application

--- app/src/main/java/com/pedriapps/androparksf/MapHome.java
+++ app/src/main/java/com/pedriapps/androparksf/MapHome.java
@@ -10,10 +10,12 @@ import android.widget.Button;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -25,7 +27,8 @@ public class MapHome
     private GoogleApiClient mGoogleApiClient;
     public static final String TAG = MapHome.class.getSimpleName();
     Location lastLocation;
-    LatLng latLng;
+    private LatLng latLng;
+    private GoogleMap theMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,11 @@ public class MapHome
                 .addApi(LocationServices.API)
                 .build();
         mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
@@ -84,6 +92,13 @@ public class MapHome
         lastLocation =
                 LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         latLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+
+        Log.i(TAG, lastLocation.toString());
+        Log.i(TAG, latLng.toString());
+
+        theMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(15).build();
+        theMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
     @Override
