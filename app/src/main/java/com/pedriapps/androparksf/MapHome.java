388186027118b0e7a52331dd1de3553f7cf2a2b7
Pedro Garate <pedro.garate@yahoo.com>
2015-10-08 20:40:35 -0700

    Update to MapHome.java -Get location using Google Play Services -Set up different markers for longClick and a button.

--- app/src/main/java/com/pedriapps/androparksf/MapHome.java
+++ app/src/main/java/com/pedriapps/androparksf/MapHome.java
@@ -1,7 +1,15 @@
 package com.pedriapps.androparksf;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -9,7 +17,15 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapHome extends FragmentActivity implements OnMapReadyCallback {
+public class MapHome
+        extends FragmentActivity
+        implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG = MapHome.class.getSimpleName();
+    Location lastLocation;
+    LatLng latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,12 @@ public class MapHome extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -27,6 +49,22 @@ public class MapHome extends FragmentActivity implements OnMapReadyCallback {
         map.getUiSettings().setCompassEnabled(true);
         map.getUiSettings().setMapToolbarEnabled(false);
 
+        Button parkHere = (Button) findViewById(R.id.park_button);
+
+        parkHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (lastLocation != null) {
+                    map.clear();
+                    map.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .draggable(false)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                            .title("Parked here"));
+                }
+            }
+        });
+
         map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
@@ -35,8 +73,26 @@ public class MapHome extends FragmentActivity implements OnMapReadyCallback {
                         .position(latLng)
                         .draggable(true)
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                        .title("Hello"));
+                        .title("Hello New Marker!"));
             }
         });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        lastLocation =
+                LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        latLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Location services failed. Please connect.");
     }
 }
